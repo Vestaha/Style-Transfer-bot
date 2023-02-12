e1004d261701012ec6e59060ecde2410e98d13b3
Vestaha <103606051+Vestaha@users.noreply.github.com>
2023-02-12 15:20:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import model
+from PIL import Image
+import size
 
 # для работы бота нам нужен токен, который мы берем в @BotFather
-TOKEN = None
 with open('token.txt') as f:
     TOKEN = f.read().strip()
 
@@ -36,9 +37,7 @@ async def photo_processing(message):
     if flag:
         await message.photo[-1].download('content.jpg')
         await message.answer(text='Первое изображение получено.'
-                                  'Отправь изображение, стиль которого ты хочешь применить к первому или '
-                                  'используй команду /cancel '
-                                  'для того, чтобы выбрать другую картинку.')
+                                  'Отправь изображение, стиль которого ты хочешь применить к первому')
         flag = False
         content_flag = True  # Бот знает, что существует изображение для стиилизации
 
@@ -61,18 +60,28 @@ async def contin(message: types.Message):
     await message.answer(text='Обработка началась и может занять некоторое время. '
                               'Подождите немного.',
                          reply_markup=types.ReplyKeyboardRemove())
-    style_img = model.image_loader('style.jpg')
-    content_img = model.image_loader('content.jpg')
+    img_for_shape = Image.open('content.jpg')
+    # Нужно для подгонки форм изображений и чтобы итоговое изображение соответствовало размеру исходного
+    (width, height) = img_for_shape.size
+    img_shape = (height, width)
+    # Далее идёт пред- и пост- обработка изображений
+    content_img = size.preprocess('content.jpg', img_shape)
+    content_img = size.postprocess(content_img)
+    content_img = model.image_loader(content_img, height)
 
-    # тут должна быть предвариетельная обработка изображений
+    style_img = size.preprocess('style.jpg', img_shape)
+    style_img = size.postprocess(style_img)
+    style_img = model.image_loader(style_img, height)
 
     input_img = content_img.clone()
+
     output_image = model.run_style_transfer(model.cnn, model.cnn_normalization_mean, model.cnn_normalization_std,
                              content_img, style_img, input_img)
-    output_pil = output_image.cpu().clone()  # we clone the tensor to not do changes on it
+    output_pil = output_image.cpu().clone()  # клонируем тензор, чтобы не производить изменения внутри
     output_pil = output_pil.squeeze(0)
     output_pil = model.unloader(output_pil)
     output_pil.save('result.jpg')
+    print(output_pil.size)
     with open('result.jpg', 'rb') as file:
         await message.answer_photo(file, caption='Done!')
 
